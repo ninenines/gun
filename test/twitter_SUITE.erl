c1298095d2b6098c32153b1cfcfe1aed8a6424ab
Josh Toft <josh@pack.io>
2014-05-12 21:33:51 -0700

    Start/stop cowlib for tests, closes #4

--- test/twitter_SUITE.erl
+++ test/twitter_SUITE.erl
@@ -32,6 +32,7 @@ all() ->
 init_per_suite(Config) ->
 	ok = application:start(ranch),
 	ok = application:start(crypto),
+	ok = application:start(cowlib),
 	ok = application:start(asn1),
 	ok = application:start(public_key),
 	ok = application:start(ssl),
@@ -43,6 +44,7 @@ end_per_suite(_) ->
 	ok = application:stop(ssl),
 	ok = application:stop(public_key),
 	ok = application:stop(asn1),
+	ok = application:stop(cowlib),
 	ok = application:stop(crypto),
 	ok = application:stop(ranch),
 	ok.
