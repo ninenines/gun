01eb4636f7a25ccdd3099629cb0d580ee8688dcc
Loïc Hoguin <essen@ninenines.eu>
2020-11-12 11:25:33 +0100

    Use gun:stream_ref() where applicable

--- src/gun_data_h.erl
+++ src/gun_data_h.erl
@@ -20,10 +20,10 @@
 
 -record(state, {
 	reply_to :: pid(),
-	stream_ref :: reference()
+	stream_ref :: gun:stream_ref()
 }).
 
--spec init(pid(), reference(), _, _, _) -> {ok, #state{}}.
+-spec init(pid(), gun:stream_ref(), _, _, _) -> {ok, #state{}}.
 init(ReplyTo, StreamRef, _, _, _) ->
 	{ok, #state{reply_to=ReplyTo, stream_ref=StreamRef}}.
 
